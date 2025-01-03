c98fa106ed2447131bb552417a5055f4924d593e
Arthur277 <arthurdeumenitsako@gmail.com>
2025-01-03 12:42:32 +0100

    alluser03012025

--- src/app/trouvercooker/page.jsx
+++ src/app/trouvercooker/page.jsx
@@ -1,4 +1,6 @@
-import { use } from 'react';
+"use client"
+import { use, useState } from 'react';
+import styles from './trouvercooker.module.css'
 
 async function getUsers() {
   const res = await fetch('http://localhost:3000/api/getAllUser', { cache: 'no-store' });
@@ -10,17 +12,45 @@ async function getUsers() {
 
 export default function Users() {
   const users = use(getUsers());
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentUsers = users.slice(indexOfFirstItem, indexOfLastItem);
+
+  const totalPages = Math.ceil(users.length / itemsPerPage);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <div>
-      <h1>Liste des utilisateurs</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            {user.prenom} {user.nom} - {user.email}
-          </li>
+      <h1 className={styles.titre}>Liste des cookers</h1>
+      <table className={styles.tableau}>
+        <thead>
+          <tr>
+            <th scope="col">Numéro</th>
+            <th scope="col">Nom</th>
+            <th scope="col">Specialité</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentUsers.map((user) => (
+            <tr key={user.id}>
+              <th scope="row">{user.id}</th>
+              <td>{user.nom}</td>
+              <td>{user.specialite}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className={styles.pagination}>
+        {Array.from({ length: totalPages }, (_, i) => (
+          <button key={i} onClick={() => paginate(i + 1)}>
+            {i + 1}
+          </button>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
